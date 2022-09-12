e7a2eea6cf2c23a7619ca335e85c81fef69e439d
j-hashimoto <35536878+joshua-hashimoto@users.noreply.github.com>
2022-09-12 20:07:46 +0900

    build

---
+++ dist/types/Gantt.d.ts
@@ -0,0 +1,46 @@
+import { Dayjs } from 'dayjs';
+import React from 'react';
+import { GanttContext } from './context';
+import './Gantt.less';
+import { DefaultRecordType, Gantt } from './types';
+export interface GanttProps<RecordType = DefaultRecordType> {
+    data: Gantt.Record<RecordType>[];
+    columns: Gantt.Column[];
+    dependencies?: Gantt.Dependence[];
+    onUpdate: (record: Gantt.Record<RecordType>, startDate: string, endDate: string) => Promise<boolean>;
+    startDateKey?: string;
+    endDateKey?: string;
+    isRestDay?: (date: string) => boolean;
+    unit?: Gantt.Sight;
+    rowHeight?: number;
+    innerRef?: React.MutableRefObject<GanttRef>;
+    getBarColor?: GanttContext<RecordType>['getBarColor'];
+    showBackToday?: GanttContext<RecordType>['showBackToday'];
+    showUnitSwitch?: GanttContext<RecordType>['showUnitSwitch'];
+    showTaskBarTotalDays?: GanttContext<RecordType>['showTaskBarTotalDays'];
+    onRow?: GanttContext<RecordType>['onRow'];
+    tableIndent?: GanttContext<RecordType>['tableIndent'];
+    expandIcon?: GanttContext<RecordType>['expandIcon'];
+    renderBar?: GanttContext<RecordType>['renderBar'];
+    renderGroupBar?: GanttContext<RecordType>['renderGroupBar'];
+    renderInvalidBar?: GanttContext<RecordType>['renderInvalidBar'];
+    renderBarThumb?: GanttContext<RecordType>['renderBarThumb'];
+    onBarClick?: GanttContext<RecordType>['onBarClick'];
+    tableCollapseAble?: GanttContext<RecordType>['tableCollapseAble'];
+    scrollTop?: GanttContext<RecordType>['scrollTop'];
+    disabled?: boolean;
+    alwaysShowTaskBar?: boolean;
+    renderLeftText?: GanttContext<RecordType>['renderLeftText'];
+    renderRightText?: GanttContext<RecordType>['renderLeftText'];
+    onExpand?: GanttContext<RecordType>['onExpand'];
+    /**
+     * 自定义日期筛选维度
+     */
+    customSights?: Gantt.SightConfig[];
+}
+export interface GanttRef {
+    backToday: () => void;
+    getWidthByDate: (startDate: Dayjs, endDate: Dayjs) => number;
+}
+declare const GanttComponent: <RecordType extends DefaultRecordType>(props: GanttProps<RecordType>) => JSX.Element;
+export default GanttComponent;
---
+++ dist/types/index.d.ts
@@ -0,0 +1,4 @@
+import GanttComponent, { GanttProps, GanttRef } from './Gantt';
+import { Gantt } from './types';
+export { GanttProps, Gantt, GanttRef };
+export default GanttComponent;
